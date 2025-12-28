58f390144cba6ec8318917240a6ca7c342119a6d
abhay <kashyapabhay1995@gmail.com>
2025-12-28 13:12:55 +0530

    "SuperContraGame class Added"

---
+++ src/main/java/com/project1/spring_framework/game/SuperContraGame.java
@@ -0,0 +1,20 @@
+package com.project1.spring_framework.game;
+
+public class SuperContraGame {
+	public void up() {
+		System.out.println("Jump");
+	}
+
+	public void down() {
+		System.out.println("Crouch");
+
+
+	}
+	public void left() {
+		System.out.println("Go back"); 	
+
+	}
+	public void right() {
+		System.out.println("Move forward");
+	}
+}
